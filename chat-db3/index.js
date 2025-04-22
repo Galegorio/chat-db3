3dbfcd8aa1827c2ed0acaa9302b698a97f27d8b7
Galegorio <167584367+Galegorio@users.noreply.github.com>
2025-04-22 20:35:16 -0300

    Começo

---
+++ chat-db3/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const ejs = require('ejs');
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view', path.join(__dirname, 'public'));
+
+app.engine('html', ejs.renderFile);
+
+app.use('/', (req, res)=>{
+    res.render('index.html')
+});
+
+server.listen(3000, ()=>{
+    console.log('Whystzap rodando - http://localhost:3000')
+
+});
+
+const favelados_volei_real_cria_rock_and = 6 //Vitor e Kauan valem por 2
