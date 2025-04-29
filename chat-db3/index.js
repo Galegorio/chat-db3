d914fe2238b5bcc80538af92a62c11d59609e409
Galegorio <167584367+Galegorio@users.noreply.github.com>
2025-04-29 20:38:51 -0300

    Add files via upload

--- chat-db3/index.js
+++ chat-db3/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const http = require('http');
 
+const socketIO = require('socket.io');
+
+const ejs = require('ejs');
+
+const path = require('path');
+
 const app = express();
 
 const server = http.createServer(app);
 
-const ejs = require('ejs');
+const io = socketIO(server)
 
-const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,8 +25,38 @@ app.use('/', (req, res)=>{
     res.render('index.html')
 });
 
+// ##### LOGICA SOCKET.IO - ENVIO E PROPAGAÇÃO DE MENSAGEM #####
+
+
+// ARRAY que simula o banco de dados
+let messages = [];
+
+
+// ESTRUTURA DE CONEXÃO DE DO SOCKET.IO 
+io.on('connection', socket=>{
+
+    // TESTE DE CONEXÂO
+    console.log('NOVO USUARIO CONECTADO: ' + socket.id);
+
+    //Recupera e mantém(exibe) as mensagens entre o front e o back:
+    socket.emit('previousMessage', messages);
+
+    //Logica de chat quando a mensagem é enviada:
+    socket.on('sendMessage', data=>{
+
+        //Adiciona no final do array de mensagens:
+        messages.push(data);
+
+        socket.broadcast.emit('receivedMessage', data);
+
+    });
+
+});
+
+
 server.listen(3000, ()=>{
-    console.log('Whystzap rodando - http://localhost:3000')
+    console.log('Whystzap rodando - http://localhost:3000');
+
 
 });
 
